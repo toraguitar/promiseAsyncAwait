0dcd42d89eed5d37ea01ed228a89a3071527539a
工藤 景虎 <kagetora_kudo@waku-2.com>
2023-03-10 20:01:46 +0900

    fetchHello.jsの完成

--- fetchHello.js
+++ fetchHello.js
@@ -1,10 +1,8 @@
-/**
- * jsonファイルからデータを取得し、画面に出力する関数
- * @returns {void}
- */
-function displayMessage () {
-    const response = fetch('hello.json');
-    console.log(response);
+async function displayMessage () {
+    const response = await fetch('hello.json');
+    const data = await response.json(response);
+    const element = document.getElementById('message');
+    element.innerHTML = data.message;
 };
 
 displayMessage();
